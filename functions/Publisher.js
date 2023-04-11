c70e3c189944d6d015f85adef8868d43ec9eae2a
Bohdan Basov <bohdan.basov@techmagic.co>
2023-04-12 00:09:33 +0300

    Initital commit

---
+++ functions/Publisher.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk')
+
+const TOPIC_ARN = process.env.TOPIC_TOPIC01
+
+module.exports.handler = async (event, context) => {
+  const sns = new AWS.SNS()
+
+  await sns
+    .publish({
+      Message: { event: event.body, eventId: context.awsRequestId, functionName: context.functionName },
+      TopicArn: TOPIC_ARN,
+    })
+    .promise()
+
+  return {
+    statusCode: 201,
+  }
+}
